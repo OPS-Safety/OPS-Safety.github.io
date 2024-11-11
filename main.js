98d4555c51a1ed31c3da8fb2753d15df9fa86275
AdamA40K <aatta058@uottawa.ca>
2024-11-11 15:54:22 -0500

    Finishing the Demo Page and changing the size and place of the text

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Map
-const mymap = L.map('map').setView([45.421451317787124, -75.68283615865924], 13);
+const mymap = L.map('map').setView([45.43570480978292, -75.67560242175543], 14);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -30,6 +30,44 @@ const circle = L.circle([45.43997982726112, -75.6657004625648], {
     fillOpacity:0.2
 }).addTo(mymap).bindPopup("<h4>Dangerous area</h4>")
 
+
+
+const circle1 = L.circle([45.425424881797206, -75.68935052719488], {
+    radius:300,
+    color:"green",
+    fillColor: "green",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Police Stations near</h4>")
+
+const circle2 = L.circle([45.42248387314212, -75.69710264614643], {
+    radius:300,
+    color:"green",
+    fillColor: "green",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Police Stations near</h4>")
+
+const circle3 = L.circle([45.425996699571925, -75.68396846293271], {
+    radius:300,
+    color:"green",
+    fillColor: "green",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Fire Stations near</h4>")
+
+const circle4 = L.circle([45.434977128131045, -75.6908676329835], {
+    radius:300,
+    color:"green",
+    fillColor: "green",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Fire Stations near</h4>")
+
+
+const circle5 = L.circle([45.443755508480606, -75.66770764074332], {
+    radius:300,
+    color:"green",
+    fillColor: "green",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Fire Stations near</h4>")
+
     // Polygon
 
 const polygon = L.polygon([
@@ -43,6 +81,69 @@ const polygon = L.polygon([
     fillColor:"blue",
     fillOpacity:0.2
 }).addTo(mymap).bindPopup("<h4>Known drugdealing gang in this area</h4>")
+
+
+const polygon1 = L.polygon([
+    [45.42627342166855, -75.68797395823081],
+    [45.42627342166855, -75.68779020785462],
+    [45.426178166391836, -75.68769415652162],
+    [45.42614006423614, -75.687819440869],
+    [45.42627342166855, -75.68797395823081],
+], {
+    color:"blue",
+    fillColor:"blue",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Know homeless meet up area</h4>")
+
+const polygon2 = L.polygon([
+    [45.42553645559151, -75.69307571544098],
+    [45.425712734537775, -75.69321525366583],
+    [45.43018401667419, -75.6826871432304],
+    [45.43002182770806, -75.68252206379348],
+    [45.42553645559151, -75.69307571544098],
+], {
+    color:"yellow",
+    fillColor:"yellow",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Street frequented by people under the influence of drugs</h4>")
+
+const polygon3 = L.polygon([
+    [45.42819006203674, -75.6875031662516],
+    [45.42869810555097, -75.68788052644287],
+    [45.42926019091928, -75.68642114366233],    
+    [45.428781878244465, -75.68612849698339],
+    [45.42819006203674, -75.6875031662516],
+], {
+    color:"blue",
+    fillColor:"blue",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>An unsafe shopping centre</h4>")
+
+const polygon4 = L.polygon([
+    [45.4294778077171, -75.68399778845692],
+    [45.429559516112356, -75.68363556429465],
+    [45.429350705533736, -75.68344582782869],
+    [45.42922662931683, -75.68370887156559],
+    [45.4294778077171, -75.68399778845692],
+], {
+    color:"blue",
+    fillColor:"blue",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Know homeless meet up area</h4>")
+
+const polygon5 = L.polygon([
+    [45.426110668070585, -75.68591005638599],
+    [45.426178673308286, -75.68575331077473],
+    [45.42603466211969, -75.68561936452512],
+    [45.425982657989096, -75.68578750981719],
+    [45.426110668070585, -75.68591005638599],
+], {
+    color:"blue",
+    fillColor:"blue",
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup("<h4>Known drugdealing gang in this house</h4>")
+
+
 
 //Live alert reporting feature
 const reports = JSON.parse(localStorage.getItem('reports')) || [];
